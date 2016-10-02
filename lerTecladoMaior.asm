f7501cc25926310aaf733484699992b616f77d1f
osmirmariano <osmirmariano@uft.edu.br>
2016-10-02 13:59:29 -0300

    Implementações

--- lerTecladoMaior.asm
+++ lerTecladoMaior.asm
@@ -1,4 +1,5 @@
 	.data
+pergunta: .asciiz "Informe um valor inteiro: " 
 msg1:	.asciiz "A é maior que C"
 msg2:	.asciiz "C é maior que A"
 msg3: 	.asciiz "A é igual a C"
@@ -6,40 +7,46 @@ msg3: 	.asciiz "A é igual a C"
 	.globl inicio
 inicio:
 	jal le_inteiro_do_teclado  # chama função para ler
-	la  $t0, 0($v0)		   # carrega o inteiro lido em $t7
+	#la  $t0, 0($v0)		   # carrega o inteiro lido em $t7
 	#jal le_inteiro_do_teclado  # chama função para ler
-	la  $t1, 0($v1)	
-	jal chama
+	#la  $t1, 0($v1)	
+	j chama
 	
 	
 le_inteiro_do_teclado:
+	li $v0, 4
+	la $a0, pergunta
+	syscall
 	li $v0, 5
-	li $v1, 5	# código para ler um inteiro
-	syscall		# executa a chamada do SO para ler
-	jr $ra		# volta para o lugar de onde foi chamado (no caso, jal le_inteiro_do_teclado)
+	syscall
+	add $t2, $zero, $v0
+	li $v0, 5
+	syscall
+	add $t3, $zero, $v0
+	j voltar
+voltar: 
+	jr $ra	
 	
 chama:
-	li $v0, 1
-	la $a0, ($t0)
-	la $a1, ($t1)
 	
-	ble $a0, $a1, Amaior #Maior
+	ble $t2, $t3, Amaior #Maior
 	la $a0, msg1
 	li $v0, 4
 	syscall
-	jr $ra
+	#jr $ra
 	
 Amaior:
-	ble $a1, $a0,  Bmaior#Maior
+	ble $t3, $t2,  Bmaior#Maior
 	la $a0, msg2
 	li $v0, 4
     	syscall
-    	jr $ra
+    	#jr $ra
     	
 Bmaior:
+	bne $t2, $t3, encerra
 	la $a0, msg3
 	li $v0, 4
     	syscall
-    	jr $ra
-    		
+    	#jr $ra
+encerra:
 	
