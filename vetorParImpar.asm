acf3e8ef0d004181ae1cc8bbd6875d46a4e5958d
osmirmariano <osmirmariano@uft.edu.br>
2016-10-02 14:34:17 -0300

    Implementação novos exercícios

---
+++ vetorParImpar.asm
@@ -0,0 +1,5 @@
+#Fazer um programa leia um vetor, mostre todos os números pares desse vetor, depois mostre todos
+#os números ímpares e diga se no vetor há mais números pares ou números ímpares.
+
+	.data
+vetor:  .word 0,1,2,3,4,5,6,7,8,9
