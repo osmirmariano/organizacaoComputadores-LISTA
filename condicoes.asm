94654be8583b1339e3a2c458262056df560e5615
osmirmariano <osmirmariano@uft.edu.br>
2016-10-02 00:55:09 -0300

    Implementação

--- condicoes.asm
+++ condicoes.asm
@@ -1,4 +1,4 @@
-		.data
+	.data
 msg1:	.asciiz "A é maior que C"
 msg2:	.asciiz "C é maior que A"
 msg3: 	.asciiz "A é igual a C"
@@ -10,30 +10,23 @@ c: 	.word 2
 	lw $a0, a
 	lw $a1, c
 	li $v0, 1
-	blt $a0, $a1, menor #Menor
-	bgt $a0, $a1, maior #Maior
-	beq $a0, $a1, iguais #Iguais
-menor:
+
+	ble $a0, $a1, Amaior #Maior
+	la $a0, msg1
+	li $v0, 4
+	syscall
+	jr $ra
+	
+Amaior:
+	ble $a1, $a0,  Bmaior#Maior
 	la $a0, msg2
 	li $v0, 4
     	syscall
     	jr $ra
-maior:
-	la $a0, msg1
+    	
+Bmaior:
+	la $a0, msg3
 	li $v0, 4
     	syscall
     	jr $ra
-iguais:
-	la $t0, msg3
-	li $v0, 4
-    	syscall
-    	jr $ra
-	
-	
-#	lw $a0, x
-#	li $v0, 1
-#	syscall
-#	la $a0, msg
-#	li $v0, 4 
-#	syscall
-#	jr $ra
+    	
